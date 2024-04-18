5bdcb0768839eee32562f164082cc6aa6fd81cbd
Chetanveer <gobinashnil@gmail.com>
2024-04-18 12:44:56 +0000

    commented useless part of code. to move to tests

--- scripts/network_interface.py
+++ scripts/network_interface.py
@@ -22,13 +22,13 @@ class NetworkInterface:
 
 # Example usage
 
-interface_name = "enp2s0"
-#interface_name = "enx3c18a0b38076"
-interface = NetworkInterface(interface_name) # Replace with your interface name
-if interface.check_interface_exists():
-    if interface.check_interface_up():
-        print(f"Interface {interface.interface_name} exists and is up.")
-    else:
-        print(f"Interface {interface.interface_name} exists but is not up.")
-else:
-    print(f"Interface {interface.interface_name} does not exist.")
+# interface_name = "enp2s0"
+# #interface_name = "enx3c18a0b38076"
+# interface = NetworkInterface(interface_name) # Replace with your interface name
+# if interface.check_interface_exists():
+#     if interface.check_interface_up():
+#         print(f"Interface {interface.interface_name} exists and is up.")
+#     else:
+#         print(f"Interface {interface.interface_name} exists but is not up.")
+# else:
+#     print(f"Interface {interface.interface_name} does not exist.")
