a64b4956e1c7faa30bc6024218d771366d8fd3b6
Chetanveer <gobinashnil@gmail.com>
2024-04-18 13:22:14 +0000

    added error to account for existance of macvlan

--- scripts/macvlan.py
+++ scripts/macvlan.py
@@ -10,15 +10,25 @@ class MacVlan:
         self.set_macvlan_up()
 
     def add_macvlan(self):
-        command = ["sudo", "ip", "link", "add", self.macvlan_name, "link", self.interface_name, "type", "macvlan", "mode", "bridge"]
-        subprocess.run(command, check=True)
+        try:
+            command = ["sudo", "ip", "link", "add", self.macvlan_name, "link", self.interface_name, "type", "macvlan", "mode", "bridge"]
+            subprocess.run(command, check=True)
+        except subprocess.CalledProcessError:
+            print(f"Macvlan {self.macvlan_name} exists. Creation cannot be done.")
 
     def set_macvlan_up(self):
         command = ["sudo", "ip", "link", "set", "dev", self.macvlan_name, "up"]
         subprocess.run(command, check=True)
 
-# Example usage
-interface_name = "enp2s0"
-macvlan = "macvlan1"
-creator = MacVlan(interface_name, macvlan)
-creator.create_macvlan()
+    def macvlan_exists(self):
+        try:
+            subprocess.check_output(["ip", "link", "show", self.macvlan_name], stderr=subprocess.STDOUT)
+            return True
+        except subprocess.CalledProcessError:
+            return False
+
+# # Example usage
+# interface_name = "enp2s0"
+# macvlan = "macvlan1"
+# creator = MacVlan(interface_name, macvlan)
+# creator.create_macvlan()
