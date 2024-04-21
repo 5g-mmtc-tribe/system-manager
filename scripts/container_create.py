adea2ace65028385122dfdd4df2ec477d2d8e52f
Chetanveer <gobinashnil@gmail.com>
2024-04-21 08:13:47 +0000

    container creation and deletion

---
+++ scripts/container_create.py
@@ -0,0 +1,28 @@
+import subprocess
+
+
+class Container:
+
+    def __init__(self, distribution,container_name):
+        self.distribution = distribution
+        self.container_name = container_name
+
+
+
+    def start_container(self, distribution, container_name):
+        command = ['lxc', 'launch', distribution, container_name]
+        subprocess.run(command, check=True)
+
+    def delete_container(self, container_name):
+        command = ['lxc', 'delete', container_name, '--force']
+        subprocess.run(command, check=True)
+
+distribution = 'ubuntu:22.04'
+container_name = 'testcontainers'
+
+container = Container(distribution, container_name)
+
+container.start_container(distribution,container_name)
+print("container started successfully")
+container.delete_container(container_name)
+print("container deleted")
