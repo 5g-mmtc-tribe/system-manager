d8c0ce71a882dd9b8a88b04e4dea0811c6fb6c54
Chetanveer <gobinashnil@gmail.com>
2024-05-15 15:00:30 +0200

    added class for switch manager

---
+++ switch/cisco_switch_controller.py
@@ -0,0 +1,39 @@
+from netmiko import ConnectHandler
+
+class SwitchManager():
+    def __init__(self, device_type, ip, port, password):
+        self.esw = {
+            'device_type': device_type,
+            'ip': ip,
+            'port': port,
+            'password': password,
+        }
+
+    def connect(self):
+        try:
+            net_connect = ConnectHandler(**self.esw)
+            return net_connect
+        except ConnectionRefusedError:
+            print("Connection refused. Check Telnet service and configuration.")
+            return None
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
+
+    def execute_command(self, command):
+        if self.connect() is not None:
+            output = self.connect().send_command(command)
+            self.connect().disconnect()
+            return output
+        else:
+            return "Failed to execute command."
+
+# Example usage
+
+device_type='cisco_ios_telnet'
+ip='192.168.0.30' 
+port=23 
+password='tribe'
+switch_manager = SwitchManager(device_type, ip, port, password)
+output = switch_manager.execute_command('show ip int brief')
+print(output)
