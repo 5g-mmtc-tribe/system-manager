472cd433b903ac37c8007a89ef6d3575e9f1ac70
Chetanveer <gobinashnil@gmail.com>
2024-05-16 10:12:47 +0200

    removed logging

--- switch/cisco_switch_controller.py
+++ switch/cisco_switch_controller.py
@@ -1,4 +1,10 @@
 from netmiko import ConnectHandler
+import logging
+from datetime import datetime
+
+
+logging.basicConfig(filename='netmiko_global.log', level=logging.DEBUG)
+logger = logging.getLogger("netmiko")
 
 class SwitchManager():
     def __init__(self, device_type, ip, port, password):
@@ -7,6 +13,7 @@ class SwitchManager():
             'ip': ip,
             'port': port,
             'password': password,
+            "session_log": 'netmiko_session.log'
         }
 
     def connect(self):
@@ -20,6 +27,18 @@ class SwitchManager():
             print(f"Error: {e}")
             return None
 
+    def enable_switch(self):
+        try:
+            net_connect = self.connect()
+            if net_connect:
+                net_connect.enable()
+                return net_connect
+            else:
+                return None
+        except Exception as e:
+            print(f"Error enabling switch: {e}")
+            return None
+
     def execute_command(self, command):
         if self.connect() is not None:
             output = self.connect().send_command(command)
@@ -27,6 +46,10 @@ class SwitchManager():
             return output
         else:
             return "Failed to execute command."
+
+    
+
+    
 
 # Example usage
 
@@ -37,3 +60,10 @@ password='tribe'
 switch_manager = SwitchManager(device_type, ip, port, password)
 output = switch_manager.execute_command('show ip int brief')
 print(output)
+#print("trying 1")
+#switch_manager.connect()
+
+print("testing...")
+#switch_manager.connect_enable()
+net_connect = switch_manager.enable_switch()
+
