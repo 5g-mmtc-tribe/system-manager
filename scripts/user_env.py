031be8a070ec6c4501d84c332892d3039fc33d64
Chetanveer <gobinashnil@gmail.com>
2024-05-24 16:20:41 +0000

    removed useless data from dataclass of user

--- scripts/user_env.py
+++ scripts/user_env.py
@@ -3,11 +3,11 @@ from dataclasses import dataclass
 
 @dataclass
 class UserEnv:
-    interface_name: str = 'enp2s0'
-    macvlan_name: str = 'demomacvlan1'
-    ip_addr: str = '192.168.100.9/24'
-    distribution: str = 'ubuntu:22.04'
-    container_name: str = 'demo'
-    ip_addr_veth: str = '192.168.100.30/24'
-    bridge: str = 'lxdbr0'
-    interface_dhcp: str = 'eth1'
+    interface_name: str 
+    macvlan_name: str 
+    ip_addr: str
+    distribution: str 
+    container_name: str
+    ip_addr_veth: str 
+    bridge: str
+    interface_dhcp: str 
