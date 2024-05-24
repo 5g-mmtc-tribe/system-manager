1fbabc38283ca4d7637c4ef24d64ebcb4c4eb15a
Chetanveer <gobinashnil@gmail.com>
2024-05-24 00:24:12 +0000

    reworked api for creating user env

--- api/launch.py
+++ api/launch.py
@@ -1,36 +1,26 @@
 from fastapi import FastAPI
 from subprocess import run, CalledProcessError
 import os
+from user_env import UserEnv
+import sys
+
+# Get the absolute path of the parent directory
+script_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../scripts'))
+sys.path.append(script_path)
+# Now import the function from the script
+from create_env import main
+#from destroy_env import 
+
 app = FastAPI()
 
-@app.post("/launch_user_env")
-async def launch_script():
-    try:
-        # Construct the path to the scripts directory relative to this script
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        scripts_dir = os.path.join(script_dir, "../scripts")
+def create_env(config: UserEnv):
+    main(config)
         
-        # Construct the path to main.py
-        main_script_path = os.path.join(scripts_dir, "main.py")
-        run(["python", main_script_path], check=True)
-        return {"message": "User environment configured properly"}
-    except CalledProcessError as e:
-        return {"error": f"Error launching script: {e}"}
-
-
-
-@app.post("/destroy_user_env")
-async def launch_script():
-    try:
-        # Construct the path to the scripts directory relative to this script
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        scripts_dir = os.path.join(script_dir, "../scripts")
         
-        # Construct the path to main.py
-        destroy_script_path = os.path.join(scripts_dir, "destroy_env.py")
-        run(["python", destroy_script_path], check=True)
-        return {"message": "User env destroyed successfully"}
-    except CalledProcessError as e:
-        return {"error": f"Error launching script: {e}"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
