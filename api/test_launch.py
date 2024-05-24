353ab8580f3e3c6101d1cefeb6a37a5337f8e8d6
Chetanveer <gobinashnil@gmail.com>
2024-05-24 00:24:59 +0000

    added a sample of how to use api bu importing functions from the api

---
+++ api/test_launch.py
@@ -0,0 +1,21 @@
+from launch import create_env
+from user_env import UserEnv
+
+def main():
+
+    config = UserEnv(
+        interface_name='enp2s0',
+        macvlan_name='demomacvlan1',
+        ip_addr='192.168.100.9/24',
+        distribution='ubuntu:22.04',
+        container_name='finalTest',
+        ip_addr_veth='192.168.100.30/24',
+        bridge='lxdbr0',
+        interface_dhcp='eth1'
+    )
+
+    create_env(config)
+
+
+if __name__=="__main__":
+    main()
