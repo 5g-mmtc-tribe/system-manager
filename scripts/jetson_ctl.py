bc92e9e4540993b4a093f5dfd372e103cfbc0f8d
Chetanveer <gobinashnil@gmail.com>
2024-05-25 23:11:13 +0000

    completed jetson usb instances

--- scripts/jetson_ctl.py
+++ scripts/jetson_ctl.py
@@ -19,12 +19,14 @@ class Jetson:
                                             check = True)
 
 
-        print(instances.stdout)
+        xavier_usb_instance = instances.stdout.split("/")[5]
 
+        return xavier_usb_instance
 
 
     def list_devices(self):
         number_devices = 0
+        xavier_instances = []
         command = "lsusb"
         usb_devices = subprocess.run(command, capture_output= True, text=True, check = True)
         usb_devices = usb_devices.stdout.splitlines()
@@ -33,12 +35,20 @@ class Jetson:
             device = device.split(":",1)[1].strip()
             if device == self.xavier_id:
                 number_devices = number_devices + 1
+                xavier_instances.append(self.get_xavier_instances())
 
-        return number_devices
+        if number_devices == 0:
+            print("No Jetson Xavier connected")
+
+            return number_devices
+
+        else:  
+
+            return number_devices, xavier_instances
 
 jetson = Jetson()
 
 number = jetson.list_devices()
 print(number)
 
-jetson.get_xavier_instances()
+#jetson.get_xavier_instances()
