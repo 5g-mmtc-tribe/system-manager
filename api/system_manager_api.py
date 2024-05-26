5be86f697ead7ada037b8530b07872005cfa0f7e
Chetanveer <gobinashnil@gmail.com>
2024-05-26 10:15:17 +0000

    added more meaningful name to system manager api

--- api/system_manager_api.py
+++ api/system_manager_api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from subprocess import run, CalledProcessError
 import os
 import sys
+import json
 
 # Get the absolute path of the parent directory
 script_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../scripts'))
@@ -11,7 +12,6 @@ from create_env import launch_env
 from destroy_env import destroy_user_env
 from user_env import UserEnv
 from jetson_ctl import Jetson
-
 
 
 app = FastAPI()
@@ -24,14 +24,31 @@ def destroy_env(config: UserEnv):
     destroy_user_env(config)
 
 
-def get_jetsons_list():
+def get_active_jetsons_list():
     jetson = Jetson()
     print(jetson.list_devices())
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+def get_resource_list():
+    # Get the absolute path to the resource.json file
+    current_dir = os.path.dirname(__file__)
+    data_dir = os.path.join(current_dir, '../data')
+    json_path = os.path.join(data_dir, 'resource.json')
+
+
+
+    # Read the JSON file
+    with open(json_path, 'r') as file:
+        data = json.load(file)
+        
+    return data
+
+#get_resource_list()
+
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
