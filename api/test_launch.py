7890297945df0c032f892d581f3beb76347625bf
Chetanveer <gobinashnil@gmail.com>
2024-05-26 10:16:06 +0000

    new way to use api

--- api/test_launch.py
+++ api/test_launch.py
@@ -1,21 +1,24 @@
-from launch import create_env, destroy_env
-from user_env import UserEnv
+#from user_env import UserEnv
 
+
+import system_manager_api as sm
 def main():
 
-    config = UserEnv(
-        interface_name='enp2s0',
-        macvlan_name='demomacvlan1',
-        ip_addr='192.168.100.9/24',
-        distribution='ubuntu:22.04',
-        container_name='finalTest',
-        ip_addr_veth='192.168.100.30/24',
-        bridge='lxdbr0',
-        interface_dhcp='eth1'
-    )
+    # config = UserEnv(
+    #     interface_name='enp2s0',
+    #     macvlan_name='demomacvlan1',
+    #     ip_addr='192.168.100.9/24',
+    #     distribution='ubuntu:22.04',
+    #     container_name='finalTest',
+    #     ip_addr_veth='192.168.100.30/24',
+    #     bridge='lxdbr0',
+    #     interface_dhcp='eth1'
+    # )
 
-    create_env(config)
+    #create_env(config)
     #destroy_env(config)
+
+    sm.get_active_jetsons_list()
     
 
 
