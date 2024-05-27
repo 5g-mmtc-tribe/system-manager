f2481dbf10c949a4944256d902dcae116c08bfe8
Chetanveer <gobinashnil@gmail.com>
2024-05-27 10:57:54 +0000

    wip to manage power in all the switches

--- switch/poe_manager.py
+++ switch/poe_manager.py
@@ -1,4 +1,54 @@
 from switch_manager import SwitchManager
+import time
+import os
+import sys
+import json
+
+class PoeManager:
+
+    def __init__(self, switch):
+        self.switch = switch
+   
+
+    def get_switch_interfaces(self):
+
+        interfaces = []
+        # Get the absolute path of the parent directory
+        script_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data'))
+        # Now include the directory path in the file path
+        file_path = os.path.join(script_path, 'resource.json')
+
+        # Open and read the JSON file
+        with open(file_path, 'r') as file:
+            resources = json.load(file)
+
+            for resource in resources:
+                print(resource)
+                interfaces.append(resource['switch_interface'])
+
+
+        return interfaces
+
+    def turn_all_off(self):
+
+        interfaces = self.get_switch_interfaces()
+
+        for interface in interfaces:
+            print(interface)
+            switch.poe_off(interface)
+            time.sleep(5)
+
+
+
+    def turn_all_on(self):
+
+        interfaces = self.get_switch_interfaces()
+
+        for interface in interfaces:
+            print(interface)
+            switch.poe_on(interface)
+            time.sleep(5)
+
 
 
 
@@ -18,12 +68,40 @@ switch = SwitchManager(device_type = device['device_type'],
 
 
 
-#out = switch_obj.sendCommand('show ip int bri')
-#print(out)
-# # # poe
-interface = "GigabitEthernet 1/0/11"
-# #interface = "GigabitEthernet 1/0/21"
 
-switch.poe_off(interface)
-#switch.poe_on(interface)
+power = PoeManager(switch)
+
+switch_interfaces = power.get_switch_interfaces()
+print(switch_interfaces)
+
+
+
+# #out = switch_obj.sendCommand('show ip int bri')
+# #print(out)
+# # # # poe
+# interface_1 = "GigabitEthernet 1/0/11"
+# interface_2 = "GigabitEthernet 1/0/13"
+# interfaces = [interface_1, interface_2]
+
+
+# def turn_all_off():
+#     for interface in interfaces:
+#         print(interface)
+#         switch.poe_off(interface)
+#         time.sleep(5)
+
+
+
+# def turn_all_on():
+#     for interface in interfaces:
+#         print(interface)
+#         switch.poe_on(interface)
+#         time.sleep(5)
+
+# switch.poe_off(interface_2)
+# #switch.poe_on(interface)
+
+# #turn_all_off()
+
+# #turn_all_on()
 
