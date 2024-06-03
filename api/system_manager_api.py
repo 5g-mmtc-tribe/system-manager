02c66caeb44f095a87548424c3247370321fefde
Chetanveer <gobinashnil@gmail.com>
2024-06-03 10:57:09 +0000

    implemented function to create user env as a VM

--- api/system_manager_api.py
+++ api/system_manager_api.py
@@ -177,19 +177,8 @@ def destroy_user_env_vm(vm_name, macvlan_name):
 
 
 
-def create_user_env_vm():
-    # Define the parameters
-    ubuntu_version = "24.04"
-    vm_name = "testvm"
-    root_size = "4GiB"
-    user_info = {
-            "user_name": "testvm",
-            "user_network_id": 75,
-            "user_subnet": "192.168.75.0/24",
-            "nfs_ip_addr": "192.168.75.1/24",
-            "macvlan_interface": "macvlan_testvm",
-            "macvlan_ip_addr": "192.168.75.2/24"
-        }
+def create_user_env_vm(ubuntu_version, vm_name, root_size, user_info):
+    
     # print(user_info)
 
     # Extracting the information
@@ -224,23 +213,22 @@ def flash_jetson(usb_instance):
 # ----------------------
 # Creating user env
 
-
-create_user_env_vm()
-
-
-
-
-
-
-
-
-
-
+# Define the parameters
+ubuntu_version = "24.04"
+vm_name = "testvm"
+root_size = "4GiB"
+user_info = {
+        "user_name": "testvm",
+        "user_network_id": 75,
+        "user_subnet": "192.168.75.0/24",
+        "nfs_ip_addr": "192.168.75.1/24",
+        "macvlan_interface": "macvlan_testvm",
+        "macvlan_ip_addr": "192.168.75.2/24"
+    }
+#create_user_env_vm(ubuntu_version, vm_name, root_size, user_info)
 
 # --------------------------
-
-
-
+#
 # --------------
 # Destroying user env
 
