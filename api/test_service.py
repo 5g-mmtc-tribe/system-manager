ac51c04c06e7d2aad14d65b4f5eb2c7c132d6467
Chetanveer <gobinashnil@gmail.com>
2024-06-04 22:33:06 +0000

    added examples for using api

--- api/test_service.py
+++ api/test_service.py
@@ -5,26 +5,40 @@
 import requests
 import json
 
-#--------------------------------------------------
 
-#response = requests.post('http://127.0.0.1:8000/my_endpoint', data=json.dumps(data), headers=headers)
-response = requests.post('http://127.0.0.1:8000/get_active_jetsons_list')
-print(response.status_code)
-print(response.content)
 
 #--------------------------------------------------
+
+## Get resource list
 
 response = requests.post('http://127.0.0.1:8000/get_resource_list')
 print(response.status_code)
 print(response.content)
 
-#--------------------------------------------------
+# #-------------------------------------------------
 
-data = {'user': "no-user", "request": "nothing"}
-#headers = {'Content-Type': 'application/json'}
 
-response = requests.post('http://127.0.0.1:8000/create_env', json=data) #, headers=headers)
+## Creating new users
+
+data = {
+    'user_name': "fernando",
+    'user_network_id': '7'
+}
+
+response = requests.post('http://127.0.0.1:8000/create_user', json=data)
 print(response.status_code)
-print(response.content)
+print(response.json())
 
 #-------------------------------------------------------------------------------------------------------------------
+
+
+# Destroying user environment VM
+
+# data = {
+#     'vm_name': 'testvm',
+#     'macvlan_interface': 'macvlan_testvm'
+# }
+
+# response = requests.post('http://127.0.0.1:8000/destroy_env_vm', json=data)
+# print(response.status_code)
+# print(response.json())
