faa04a99b9d02d884c44ba9ecad5d5783097123f
Chetanveer <gobinashnil@gmail.com>
2024-06-05 01:18:35 +0000

    commented clearing active users

--- api/test_service.py
+++ api/test_service.py
@@ -32,11 +32,12 @@ print(response.json())
 
 #-------------------------------------------------------------------------------------------------------------------
 # Clearing active users
+#-------------------------------------------------------------------------------------------------------------------
 
 
-# response = requests.post('http://127.0.0.1:8000/clear_active_users')
-# print(response.status_code)
-# print(response.json())
+response = requests.post('http://127.0.0.1:8000/clear_active_users')
+print(response.status_code)
+print(response.json())
 
 #-------------------------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------------------------
@@ -64,16 +65,16 @@ print(response.json())
 # #-----------------------------------------------------------------------
 # # Getting user info
 # #-----------------------------------------------------------------------
-data = {
-    'user_name': "mehdi",
-    'user_network_id': '90'
-}
+# data = {
+#     'user_name': "mehdi",
+#     'user_network_id': '90'
+# }
 
-response_user_info = requests.post('http://127.0.0.1:8000/get_user_info', json=data)
-print(response_user_info.status_code)
-print(response_user_info.json())
-# Convert JSON string to dictionary
-user_info_data = json.loads(response_user_info.json())
+# response_user_info = requests.post('http://127.0.0.1:8000/get_user_info', json=data)
+# print(response_user_info.status_code)
+# print(response_user_info.json())
+# # Convert JSON string to dictionary
+# user_info_data = json.loads(response_user_info.json())
 
 
 
@@ -96,20 +97,19 @@ user_info_data = json.loads(response_user_info.json())
 # Creating user env
 #----------------------------------------------------
 
+# data = {
+#     'ubuntu_version': '24.04',
+#     'vm_name': user_info_data['user_name'],
+#     'root_size': '4GiB',
+#     'user_info': user_info_data
+# }
 
-data = {
-    'ubuntu_version': '24.04',
-    'vm_name': user_info_data['user_name'],
-    'root_size': '4GiB',
-    'user_info': user_info_data
-}
+# # Create Pydantic model instance
+# request_data = CreateUserEnvVMRequest(**data)
 
-# Create Pydantic model instance
-request_data = CreateUserEnvVMRequest(**data)
-
-# Send request
-response = requests.post('http://127.0.0.1:8000/create_user_env_vm', json=request_data.dict())
-print(response.status_code)
-print(response.json())
+# # Send request
+# response = requests.post('http://127.0.0.1:8000/create_user_env_vm', json=request_data.dict())
+# print(response.status_code)
+# print(response.json())
 
 #----------------------------------------------------
