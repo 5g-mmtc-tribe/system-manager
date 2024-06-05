f87800068ea699517837d0f375a2b2fc86a78312
Chetanveer <gobinashnil@gmail.com>
2024-06-05 07:22:41 +0000

    moved functions at the bottom of the script

--- api/test_service.py
+++ api/test_service.py
@@ -41,7 +41,64 @@ print(response.status_code)
 print(response.json())
 
 
+# #-----------------------------------------------------------------------
+# # Getting user info
+# #-----------------------------------------------------------------------
+data = {
+    'user_name': "ferna",
+    'user_network_id': '90'
+}
 
+response_user_info = requests.post('http://127.0.0.1:8000/get_user_info', json=data)
+print(response_user_info.status_code)
+print(response_user_info.json())
+# Convert JSON string to dictionary
+user_info_data = json.loads(response_user_info.json())
+
+
+
+#----------------------------------------------------
+# Creating user env
+#----------------------------------------------------
+
+# user_env_info = {
+#     'ubuntu_version': '24.04',
+#     'vm_name': user_info_data['user_name'],
+#     'root_size': '4GiB',
+#     'user_info': user_info_data
+# }
+
+# # Create Pydantic model instance
+# request_data = CreateUserEnvVMRequest(**user_env_info)
+
+# # Send request
+# response = requests.post('http://127.0.0.1:8000/create_user_env_vm', json=request_data.dict())
+# print(response.status_code)
+# print(response.json())
+
+
+
+#-------------------------------------------------------------------------------------------------------------------
+# Destroying user environment VM
+#-------------------------------------------------------------------------------------------------------------------
+data = {
+    'vm_name': user_info_data['user_name'],
+    'macvlan_interface': user_info_data['macvlan_interface']
+}
+
+response = requests.post('http://127.0.0.1:8000/destroy_env_vm', json=data)
+print(response.status_code)
+print(response.json())
+
+
+
+#--------------------------------------------------------------------------------------------------------------------
+## Get resource list
+#--------------------------------------------------------------------------------------------------------------------
+
+# response = requests.post('http://127.0.0.1:8000/get_resource_list')
+# print(response.status_code)
+# print(response.content)
 
 
 
@@ -60,67 +117,3 @@ print(response.json())
 # response = requests.post('http://127.0.0.1:8000/testbed_reset')
 # print(response.status_code)
 # print(response.json())
-
-
-
-# #-----------------------------------------------------------------------
-# # Getting user info
-# #-----------------------------------------------------------------------
-data = {
-    'user_name': "ferna",
-    'user_network_id': '90'
-}
-
-response_user_info = requests.post('http://127.0.0.1:8000/get_user_info', json=data)
-print(response_user_info.status_code)
-print(response_user_info.json())
-# Convert JSON string to dictionary
-user_info_data = json.loads(response_user_info.json())
-
-
-
-
-#-------------------------------------------------------------------------------------------------------------------
-# Destroying user environment VM
-#-------------------------------------------------------------------------------------------------------------------
-# data = {
-#     'vm_name': user_info_data['user_name'],
-#     'macvlan_interface': user_info_data['macvlan_interface']
-# }
-
-# response = requests.post('http://127.0.0.1:8000/destroy_env_vm', json=data)
-# print(response.status_code)
-# print(response.json())
-
-
-
-#----------------------------------------------------
-# Creating user env
-#----------------------------------------------------
-
-user_env_info = {
-    'ubuntu_version': '24.04',
-    'vm_name': user_info_data['user_name'],
-    'root_size': '4GiB',
-    'user_info': user_info_data
-}
-
-# Create Pydantic model instance
-request_data = CreateUserEnvVMRequest(**user_env_info)
-
-# Send request
-response = requests.post('http://127.0.0.1:8000/create_user_env_vm', json=request_data.dict())
-print(response.status_code)
-print(response.json())
-
-
-
-
-
-#--------------------------------------------------------------------------------------------------------------------
-## Get resource list
-#--------------------------------------------------------------------------------------------------------------------
-
-# response = requests.post('http://127.0.0.1:8000/get_resource_list')
-# print(response.status_code)
-# print(response.content)
