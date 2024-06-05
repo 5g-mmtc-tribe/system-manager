2615c08b4eec900ef54c10cb7dd8650daa49e4a6
Chetanveer <gobinashnil@gmail.com>
2024-06-05 01:16:16 +0000

    more or less working version of models

--- api/models.py
+++ api/models.py
@@ -9,4 +9,18 @@ class CreateUser(BaseModel):
     user_name: str
     user_network_id: int
 
-    
+
+class UserNetworkInfo(BaseModel):
+    user_name: str
+    user_network_id: int
+    user_subnet: str
+    nfs_ip_addr: str
+    macvlan_interface: str
+    macvlan_ip_addr: str
+
+class CreateUserEnvVMRequest(BaseModel):
+    ubuntu_version: str 
+    vm_name: str
+    root_size: str
+    user_info: UserNetworkInfo
+
