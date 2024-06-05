9ca8d403236c1a993162ac3d18a7b15651649ff9
Chetanveer <gobinashnil@gmail.com>
2024-06-05 00:12:45 +0000

    added example to get user info

--- api/test_service.py
+++ api/test_service.py
@@ -22,8 +22,8 @@ print(response.content)
 ## Creating new users
 
 data = {
-    'user_name': "fernando",
-    'user_network_id': '7'
+    'user_name': "mehdi",
+    'user_network_id': '90'
 }
 
 response = requests.post('http://127.0.0.1:8000/create_user', json=data)
@@ -42,17 +42,17 @@ print(response.json())
 #-------------------------------------------------------------------------------------------------------------------
 
 
-
+#-------------------------------------------------------------------------------------------------------------------
 # Destroying user environment VM
+#-------------------------------------------------------------------------------------------------------------------
+data = {
+    'vm_name': 'testvm',
+    'macvlan_interface': 'macvlan_testvm'
+}
 
-# data = {
-#     'vm_name': 'testvm',
-#     'macvlan_interface': 'macvlan_testvm'
-# }
-
-# response = requests.post('http://127.0.0.1:8000/destroy_env_vm', json=data)
-# print(response.status_code)
-# print(response.json())
+response = requests.post('http://127.0.0.1:8000/destroy_env_vm', json=data)
+print(response.status_code)
+print(response.json())
 
 
 # -------------------------------------
@@ -72,28 +72,45 @@ print(response.json())
 # print(response.json())
 
 
+
+#-----------------------------------------------------------------------
+# Getting user info
+#-----------------------------------------------------------------------
+data = {
+    'user_name': "mehdi",
+    'user_network_id': '90'
+}
+
+response = requests.post('http://127.0.0.1:8000/get_user_info', json=data)
+print(response.status_code)
+print(response.json())
+
+
+
 #----------------------------------------------------
 # Creating user env
 #----------------------------------------------------
 
-data = {
-    'ubuntu_version': '24.04',
-    'vm_name': 'testvm',
-    'root_size': '4GiB',
-    'user_info': {
-        'user_name': 'testvm',
-        'user_network_id': 75,
-        'user_subnet': '192.168.75.0/24',
-        'nfs_ip_addr': '192.168.75.1/24',
-        'macvlan_interface': 'macvlan_testvm',
-        'macvlan_ip_addr': '192.168.75.2/24'
-    }
-}
+# data = {
+#     'ubuntu_version': '24.04',
+#     'vm_name': 'testvm',
+#     'root_size': '4GiB',
+#     'user_info': {
+#         'user_name': 'testvm',
+#         'user_network_id': 75,
+#         'user_subnet': '192.168.75.0/24',
+#         'nfs_ip_addr': '192.168.75.1/24',
+#         'macvlan_interface': 'macvlan_testvm',
+#         'macvlan_ip_addr': '192.168.75.2/24'
+#     }
+# }
 
-# Create Pydantic model instance
-request_data = CreateUserEnvVMRequest(**data)
+# # Create Pydantic model instance
+# request_data = CreateUserEnvVMRequest(**data)
 
-# Send request
-response = requests.post('http://127.0.0.1:8000/create_user_env_vm', json=request_data.dict())
-print(response.status_code)
-print(response.json())
+# # Send request
+# response = requests.post('http://127.0.0.1:8000/create_user_env_vm', json=request_data.dict())
+# print(response.status_code)
+# print(response.json())
+
+#----------------------------------------------------
